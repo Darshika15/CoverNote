de9312bc2f533dffd281ff398b311fdbd0c2d5ba
AUX_ArshadM <AUX_ArshadM@AZ1400BATIGENOS.allianzlk.int>
2018-10-31 11:08:43 +0530

    started to add explicit wait

--- src/main/java/CoverNote/CoverNote_V_I/AddCoverNote.java
+++ src/main/java/CoverNote/CoverNote_V_I/AddCoverNote.java
@@ -3,7 +3,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class AddCoverNote {
 	WebDriver driver;
@@ -17,6 +19,7 @@ public class AddCoverNote {
 	private Select pages;
 	private WebElement fromNo;
 	private Select status;
+	private WebDriverWait wait;
 
 	
 	
@@ -24,8 +27,10 @@ public class AddCoverNote {
 	
 	public  AddCoverNote(WebDriver driver) {
 		this.driver=driver;
+		wait=new WebDriverWait(driver, 30);
 		
 	}
+	
 	public void pageNavigation()
 	{
 		
@@ -38,29 +43,31 @@ public class AddCoverNote {
 	
 	public void addCoverNoteDetails(String strdeptCode,String strfundType,String strbookNumPrefix,String strbookNumber,String strStatus,String strbookletNature,String strPages,String strfromNo) throws InterruptedException
 	{
+		
 		 
-		deptCode = new Select(driver.findElement(By.id("deptCode")));
-		Thread.sleep(2000);
+		//deptCode = new Select(driver.findElement(By.id("deptCode")));
+		deptCode=new Select(wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("deptCode"))));
+		//Thread.sleep(2000);
 		deptCode.selectByVisibleText(strdeptCode);
 		fundType=new Select(driver.findElement(By.id("fundType")));
-		Thread.sleep(2000);
+		//Thread.sleep(2000);
 		fundType.selectByVisibleText(strfundType);
-		Thread.sleep(2000);
+		//Thread.sleep(2000);
 		bookNumPrefix=new Select(driver.findElement(By.id("bookNumPrefix")));
-		Thread.sleep(2000);
+		//Thread.sleep(2000);
 		bookNumPrefix.selectByVisibleText(strbookNumPrefix);
 		bookNumber=driver.findElement(By.id("bookNumber"));
 		bookNumber.sendKeys(strbookNumber);
 		status = new Select(driver.findElement(By.id("status")));
-		Thread.sleep(2000);
+		//Thread.sleep(2000);
 		status.selectByVisibleText(strStatus);
 		bookletNature= new Select(driver.findElement(By.id("bookletNature")));
-		Thread.sleep(3000);
+		//Thread.sleep(3000);
 		fundType=new Select(driver.findElement(By.id("fundType")));
-		Thread.sleep(2000);
+		//Thread.sleep(2000);
 		fundType.selectByVisibleText(strfundType);
 		bookletNature.selectByVisibleText(strbookletNature);
-		Thread.sleep(2000);
+		//Thread.sleep(2000);
 		pages=new Select(driver.findElement(By.id("pages")));
 		Thread.sleep(2000);
 		pages.selectByVisibleText(strPages);
